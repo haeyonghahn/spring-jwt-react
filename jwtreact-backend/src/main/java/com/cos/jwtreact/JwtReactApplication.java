abb3c91e13257d8f8aead11ac799d64d622e5657
haeyonghahn <yong80211@gmail.com>
2022-05-27 00:31:43 +0900

    commit

--- jwtreact-backend/src/main/java/com/cos/jwtreact/JwtReactApplication.java
+++ jwtreact-backend/src/main/java/com/cos/jwtreact/JwtReactApplication.java
@@ -1,8 +1,12 @@
 package com.cos.jwtreact;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class JwtReactApplication {
 
 	public static void main(String[] args) {
-		
+		SpringApplication.run(JwtReactApplication.class, args);
 	}
 }
